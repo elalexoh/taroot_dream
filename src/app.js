c09fcb23cb461ffc0fdc1ce5f1e01e15d8f05756
Frederick <G7.alexander@gmail.com>
2019-08-20 11:32:02 -0400

    configurando entorno de webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import './main.scss';
 class Card {
 
   constructor(name, attack, defense) {
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port:7007,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+}
