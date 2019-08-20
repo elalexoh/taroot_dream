0e7c421aad30953f7d409332d55910dd7720aedd
Frederick <G7.alexander@gmail.com>
2019-08-20 10:57:52 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# card
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/Deeply/pen/xxKVype](https://codepen.io/Deeply/pen/xxKVype).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,29 @@
+class Card {
+
+  constructor(name, attack, defense) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+  }
+  getAttack(){
+    const ataque = this.attack;
+    return ataque;
+  }
+  getDefense(){
+    const defensa = this.defense;
+    return defensa;
+  }
+  getName(){
+    const nombre = this.name;
+    return nombre;
+  }
+}
+
+let user = new Card("Djaul", 800, 500);
+const attack = document.querySelector('.attack-point h2');
+const shield = document.querySelector('.shield h2');
+const name = document.querySelector('.head h1');
+attack.innerHTML = user.getAttack();
+shield.innerHTML = user.getDefense();
+name.innerHTML = user.getName();
+
